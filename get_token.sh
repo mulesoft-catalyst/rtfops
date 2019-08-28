9b965f7c6a186cfde49f1d35c1601963bf5086e2
Terry Wang <i@terry.im>
2019-08-28 11:50:34 +1000

    script to get authorization bearer token

---
+++ get_token.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+read -p "Enter username: " USERNAME
+
+# -s do not echo input
+read -s -p "Enter password: " PASSWORD
+
+printf "\n"
+
+TOKEN=$(curl -s -d "username=$USERNAME&password=$PASSWORD" "https://anypoint.mulesoft.com/accounts/login" | jq -r '.access_token')
+
+# echo doesn't handle new line well - git commit
+printf "Bearer token: $TOKEN \n"
+
+# export only works in this sub-shell and its children
+# export TOKEN
