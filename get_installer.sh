cad1585465f9cf77796238785d93e477796ae38d
Terry Wang <i@terry.im>
2019-09-01 21:25:22 +1000

    refactor installer url construction

--- get_installer.sh
+++ get_installer.sh
@@ -7,6 +7,7 @@ source env
 
 # all metadata for rtf-agent, installer, scripts and rtfctl
 ENDPOINT="https://anypoint.mulesoft.com/runtimefabric/api/downloads"
+BASE_URL="https://runtime-fabric.s3.amazonaws.com"
 
 printf "all download metadata..."
 
@@ -17,6 +18,6 @@ printf "\n"
 VERSION=$(curl -Lks -H "Authorization: Bearer $TOKEN" ${ENDPOINT} | jq -r '.installer.version')
 
 # assemble the installer url
-INSTALLER_URL="https://runtime-fabric.s3.amazonaws.com/installer/runtime-fabric-${VERSION}.tar.gz"
+INSTALLER_URL="${BASE_URL}/installer/runtime-fabric-${VERSION}.tar.gz"
 
 printf "Latest Anypoint Runtime Fabric installer: \e[96m$INSTALLER_URL \n"
