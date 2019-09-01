8627245c595ffdd162b3ad39a01de6a7c3bd4f44
Terry Wang <i@terry.im>
2019-09-01 21:33:43 +1000

    add script to get rtfctl cli util

---
+++ get_rtfctl.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# set environment variables
+source env
+
+# all metadata for rtf-agent, installer, scripts and rtfctl
+ENDPOINT="https://anypoint.mulesoft.com/runtimefabric/api/downloads"
+BASE_URL="https://runtime-fabric.s3.amazonaws.com"
+
+VERSION=$(curl -Lks -H "Authorization: Bearer $TOKEN" ${ENDPOINT} | jq -r '.rtfctl.version')
+
+# assemble the installer url
+INSTALLER_URL="${BASE_URL}/rtfctl/rtfctl-${VERSION}"
+
+echo $INSTALLER_URL
