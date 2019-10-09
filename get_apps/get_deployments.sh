4840968b689819479b600616aa1ba0ac6140e803
Terry Wang <i@terry.im>
2019-10-09 16:16:23 +1100

    add script to get deployed apps

---
+++ get_apps/get_deployments.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# set environment variables
+if [[ -f ./env ]]; then
+    source ../env
+fi
+
+ENDPOINT="https://anypoint.mulesoft.com/hybrid/api/v2/organizations/${ORG_ID}/environments/${ENV_ID}/deployments"
+
+# Get mule apps deployed to specified env
+curl -Lks ${ENDPOINT} \
+    -H "Authorization: Bearer $TOKEN" \
+    -H "Cache-Control: no-cache" \
+    | jq
