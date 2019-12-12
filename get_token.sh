d00cdd087a7072f4abc024908faf14c85b5b54a4
Terry Wang <i@terry.im>
2019-12-12 22:17:24 +1100

    placeholder to set default values using parameter expansion

--- get_token.sh
+++ get_token.sh
@@ -10,6 +10,11 @@ read -s -p "Enter password: " PASSWORD
 
 printf "\n"
 
+# set default values using parameter expansion
+# hit enter twice to save time
+# USERNAME=${USERNAME:-mule}
+# PASSWORD=${PASSWORD:-max}
+
 # get the access token
 TOKEN=$(curl -s -d "username=${USERNAME}&password=${PASSWORD}" ${ENDPOINT} | jq -r '.access_token')
 
