6bc81dea2a9400fe3254c8e195a4016965d6b87f
Terry Wang <i@terry.im>
2021-03-31 21:22:39 +1100

    improve rename_rtf.sh logic - pass RTF agent version as command line argument

--- rename_rtf/rename_rtf.sh
+++ rename_rtf/rename_rtf.sh
@@ -2,16 +2,24 @@
 
 # rename a RTF cluster in runtime manager
 
+if [[ $# -ne 1 ]]; then
+    printf "Usage: $0 <New RTF cluster name> \n"
+    printf "Example: $0 1.8.50 \n"
+    exit -1
+fi
+
+NEW_NAME=$1
+
 # set environment variables
 [[ -f ../env ]] && source ../env
 
 ENDPOINT="https://anypoint.mulesoft.com/runtimefabric/api/organizations/${ORG_ID}/fabrics/${FABRIC_ID}"
 
-printf "${ENDPOINT} \n"
+# printf "${ENDPOINT} \n"
 
 curl -Lvk ${ENDPOINT} \
     -X PATCH \
     -H "Authorization: Bearer ${TOKEN}" \
     -H "Content-Type: application/json" \
-    -d '{"name": "rtf-cluster-reborn"}' \
+    -d '{"name": "'"$NEW_NAME"'"}' \
     | jq
