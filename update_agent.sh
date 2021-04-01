4ef57ec298c63908c00fc4d355c452f859cb4e9f
Terry Wang <i@terry.im>
2021-04-01 22:37:05 +1100

    add support for updating RTF agent

---
+++ update_agent.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# update rtf agent to version specified
+
+if [[ $# -ne 1 ]]; then
+    printf "Usage: $0 <RTF Agent Version> \n"
+    exit -1
+fi
+
+AGENT_VERSION=$1
+# AGENT_VERSION=${1-}
+
+# set environment variables
+[[ -f ./env ]] && source ./env
+
+ENDPOINT="https://anypoint.mulesoft.com/runtimefabric/api/organizations/${ORG_ID}/fabrics/{FABRIC_ID}"
+
+curl -Lks ${ENDPOINT} \
+    -X PATCH \
+    -H "Authorization: Bearer $TOKEN" \
+    -H "X-ANYPNT-ORG-ID: $ORG_ID" \
+    -H "Content-Type: application/json;charset=UTF-8" \
+    --data-raw '{"id":"$ORG_ID","version":"$AGENT_VERSION"}' \
+    | jq
